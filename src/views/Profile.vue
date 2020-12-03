2efd5ae64bc29ab4372e4dc9811a927310fae17c
Daria <dmarochnik90@gmail.com>
2020-12-02 18:56:42 -0700

    added profile and settings

---
+++ src/views/Profile.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="profile">
+    <h1>My Profile</h1>
+  </div>
+</template>
---
+++ src/views/Settings.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="settings">
+    <h1>My Account Settings</h1>
+  </div>
+</template>
